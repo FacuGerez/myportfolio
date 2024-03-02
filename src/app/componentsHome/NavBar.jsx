102a36147ef259cf4d7abb08510d4966f0abe6c0
FacuGerez <facundogerezuba@hotmail.com>
2024-03-02 10:56:15 -0300

    First commit

---
+++ src/app/componentsHome/NavBar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export function NavBar() {
+    return (
+        <header>
+            <nav>
+                <Link href={}>
+                    Redireccion 1
+                </Link>
+                <Link href={}>
+                    Redireccion 2
+                </Link>
+                <Link href={}>
+                    Redireccion 3
+                </Link>
+                <Link href={}>
+                    Redireccion 4
+                </Link>
+                <Link href={}>
+                    Redireccion 5
+                </Link>
+            </nav>
+        </header>
+    )
+}
