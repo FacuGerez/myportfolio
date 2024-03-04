42087d4c58206eea6b30d0554f222a936301009c
FacuGerez <facundogerezuba@hotmail.com>
2024-03-04 09:32:36 -0300

    First commit

--- src/app/componentsHome/NavBar.jsx
+++ src/app/componentsHome/NavBar.jsx
@@ -4,19 +4,19 @@ export function NavBar() {
     return (
         <header>
             <nav>
-                <Link href={}>
+                <Link href={"https://www.youtube.com/"}>
                     Redireccion 1
                 </Link>
-                <Link href={}>
+                <Link href={"https://www.youtube.com/"}>
                     Redireccion 2
                 </Link>
-                <Link href={}>
+                <Link href={"https://www.youtube.com/"}>
                     Redireccion 3
                 </Link>
-                <Link href={}>
+                <Link href={"https://www.youtube.com/"}>
                     Redireccion 4
                 </Link>
-                <Link href={}>
+                <Link href={"https://www.youtube.com/"}>
                     Redireccion 5
                 </Link>
             </nav>
