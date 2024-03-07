f14dfdd66c9284facb3d32950b67b7335c95cde9
FacuGerez <facundogerezuba@hotmail.com>
2024-03-07 10:12:05 -0300

    adjusting bad css practices

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import {ButtonTransparent} from "@/components/ButtonTransparent";
+import {ParticlesBackground} from "@/components/ParticlesBackground";
 
 export default function Home() {
   return (
       <div className={"pages"}>
+          <ParticlesBackground number={98}/>
           <h1>Oh Hi!!, I´m <strong>Facundo Nahuel Gerez</strong>.</h1>
           <h1>I´m a Software Developer.</h1>
           <ButtonTransparent/>
