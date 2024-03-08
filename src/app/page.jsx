590ede24a200479673464c6a2437adeabdfdbca5
FacuGerez <facundogerezuba@hotmail.com>
2024-03-07 21:07:00 -0300

    prueba vercel para RAMA

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ import {ParticlesBackground} from "@/components/ParticlesBackground";
 export default function Home() {
   return (
       <div className={"pages"}>
-          <ParticlesBackground number={98}/>
-          <h1>HOLAAAAAAAAAA BEBEEEEEEEEEEE SOY YOOOOOOOOO <strong>Facundo Nahuel Gerez</strong>.<br/>I´m a Software Developer</h1>
+          <ParticlesBackground number={64}/>
+          <h1>HOLA RAMOOOOOOOOOOOOOOON MIRA ESTA PAGINAAAAAAA <strong>Facundo Nahuel Gerez</strong>.<br/>I´m a Software Developer</h1>
           <ButtonTransparent/>
       </div>
   );
